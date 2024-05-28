7d1fcebd047cb7fbd0242ff4c6ad2a8131af3e57
Phuong Tran (BM Peter) <tranphuongk9@gmail.com>
2024-05-28 15:42:07 +0700

    Add font for layout common

--- src/utils/font.ts
+++ src/utils/font.ts
@@ -7,16 +7,43 @@ export async function loadFont(fontName: string, fontUrl: string) {
   document.fonts.add(font)
 }
 
-export const pretendard = Roboto_Mono({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-roboto-mono'
+export const pretendard = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Pretendard-Light.otf',
+      weight: '300'
+    },
+    {
+      path: '../../public/fonts/Pretendard-Regular.otf',
+      weight: '400'
+    },
+    {
+      path: '../../public/fonts/Pretendard-Bold.otf',
+      weight: '700'
+    },
+    {
+      path: '../../public/fonts/Pretendard-SemiBold.otf',
+      weight: '600'
+    },
+    {
+      path: '../../public/fonts/Pretendard-Regular.otf',
+      weight: '400',
+      style: 'italic'
+    },
+    {
+      path: '../../public/fonts/Pretendard-ExtraBold.otf',
+      weight: '800'
+    }
+  ],
+  variable: '--font-pretendard',
+  display: 'swap'
 })
 
 export const roboto_mono = Roboto_Mono({
   subsets: ['latin'],
   display: 'swap',
-  variable: '--font-roboto-mono'
+  variable: '--font-roboto-mono',
+  preload: false
 })
 
 export const nanum_pen = Nanum_Pen_Script({
@@ -24,5 +51,6 @@ export const nanum_pen = Nanum_Pen_Script({
   display: 'swap',
   subsets: ['latin'],
   weight: '400',
-  variable: '--nanum_pen'
+  variable: '--nanum_pen',
+  preload: false
 })
